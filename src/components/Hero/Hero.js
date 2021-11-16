aa579e332e8d07d5a40103b0e433c8f49cb0be29
Alaw Moradi <81196313+alawmoradi665@users.noreply.github.com>
2021-11-16 11:45:51 +1100

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome <br />
+        I am Alaw [all · love], lovely to have you here!
+      </SectionTitle>
+      <SectionText>
+        Current career changer with an endless drive for learning and solving. With a background in diverse roles and environments the keys to success has been my ability to analyse, adapt and engage. 
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
